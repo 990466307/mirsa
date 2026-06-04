23325e41e6d8eefb081be2334f0706c383390c18
pwt <990466307@qq.com>
2026-06-04 15:20:13 +0800

    Stop tracking target directory

--- target/debug/deps/core-d47c26a92a337c69.d
+++
@@ -1,8 +0,0 @@
-D:\tmp\my-mir-framework\target\debug\deps\libcore-d47c26a92a337c69.rmeta: crates\core\src\lib.rs crates\core\src\collect.rs crates\core\src\mir.rs crates\core\src\cfg.rs
-
-D:\tmp\my-mir-framework\target\debug\deps\core-d47c26a92a337c69.d: crates\core\src\lib.rs crates\core\src\collect.rs crates\core\src\mir.rs crates\core\src\cfg.rs
-
-crates\core\src\lib.rs:
-crates\core\src\collect.rs:
-crates\core\src\mir.rs:
-crates\core\src\cfg.rs:
